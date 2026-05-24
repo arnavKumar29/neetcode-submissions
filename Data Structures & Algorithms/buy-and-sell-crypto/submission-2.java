5a49d798636d3477dca095db3080bb5f5ee3c3a4
Arnav Kumar <arnar.kumar29@gmail.com>
2026-05-24 20:24:58 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int n=prices.length;
+        for(int i=0;i<n;i++){
+            for(int j= i+1; j<n;j++){
+                max = Math.max(max, prices[j] - prices[i]);
+            }
+        }
+        return max;
+    }
+}
